00bfaa4f443953e97f1525d06fd365e9d24552ac
epthalite <fmy628.1998@icloud.com>
2021-09-02 00:19:59 +0900

    create_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        passwords: 'users/passwords',
+        registrations: 'users/registrations',
+
+      }
+devise_scope :user do
+  get '/users', to: 'users/sessions#new'
+  get '/users/password', to: 'users/passwords#new'
+end
+  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
